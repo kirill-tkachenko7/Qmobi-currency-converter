00596dd9b32534a6dde05e751e1da54e84112025
Kirill Tkachenko <kirill.tkachenko7@gmail.com>
2020-04-14 19:08:29 +0300

    json returns from all methods in CurrencyConverter

--- converter.py
+++ converter.py
@@ -3,6 +3,7 @@ import json
 import datetime as dt
 import os
 from utils import cache_results
+from typing import Union, Any
 
 # TODO store in env
 APP_ID = '51b21c5160074b08b63d560c40524987'
@@ -12,8 +13,10 @@ class CurrencyConverter:
     def __init__(self):
         self.connection = http.client.HTTPConnection('openexchangerates.org')
 
+    # OpenExchangeRates give hourly rates.
+    # Therefore, no need to fetch rates from it more often than once an hour.
     @cache_results(storage_time=3600)
-    def fetch_rates(self, base_currency: str, date: str = None) -> dict:
+    def fetch_USD_rates(self, date: str = None) -> dict:
         """Get all exchange rates for a given base currency for a given date.
 
         If date is not provided, return latest exchange rates.
@@ -22,18 +25,73 @@ class CurrencyConverter:
             source = f'historical/{date}.json'
         else:
             source = 'latest.json'
-        self.connection.request(
-            'GET', f'/api/{source}?app_id={APP_ID}&base={base_currency}')
-        response = self.connection.getresponse()
-        data = json.loads(response.read())
-        return data.get('rates')
+        try:
+            raise RuntimeError
+            self.connection.request(
+                'GET', f'/api/{source}?app_id={APP_ID}')
+            response = self.connection.getresponse()
+            data = json.loads(response.read())
+            return data
+        except:
+            # clear cache
+            self.fetch_USD_rates.cache[(self,date)].pop()
+            return self.build_error(
+                503,
+                'Service Unavailable',
+                'Failed to get response from downstream server'
+            ) 
 
-    def get_rate(self, currency_from: str, currency_to: str, 
-                 date: str = None) -> float:
-        rates = self.fetch_rates(currency_from, date)
-        return rates.get(currency_to)
+    def convert(self, amount: float=1, base_currency: str = 'USD', 
+                quote_currency: str = 'RUB', date: str = None) -> dict:
+        rates = self.fetch_USD_rates(date)
+        if 'error' in rates:
+            return rates
+        
+        from_rate = rates.get(base_currency)
+        if not from_rate:
+            return self.build_error(
+                404,
+                'base_currency rate not found',
+                'Make sure you specify currency in a 3-letter ISO format'
+            )
+        to_rate = rates.get(quote_currency)
+        if not to_rate:
+            return self.build_error(
+                404,
+                'quote_currency rate not found',
+                'Make sure you specify currency in a 3-letter ISO format'
+            )
 
-    def convert(self, amount: float=1, currency_from: str = 'USD', 
-                currency_to: str = 'RUB', date: str = None) -> float:
-        rate = self.get_rate(currency_from, currency_to, date)
-        return amount * rate
+        rate = to_rate/from_rate
+        converted_amount = rate * amount
+
+        return self.build_response(
+            amount,
+            base_currency,
+            quote_currency,
+            date,
+            rate,
+            converted_amount
+        )
+
+    def build_error(self, status: int, message: str, description: str) -> dict:
+        error = {
+            'error': True,
+            'status': status,
+            'message': message,
+            'description': description,
+        }
+        return error
+    
+    def build_response(self, amount: int, base_currency: str, 
+                       quote_currency: str, date: str, exchange_rate: float, 
+                       converted_amount: float) -> dict:
+        response = {
+            'date': date,
+            'base': base_currency,
+            'quote': quote_currency,
+            'rate': exchange_rate,
+            'base_amount': amount,
+            'quote_amount': converted_amount
+        }
+        return response
--- utils.py
+++ utils.py
@@ -1,7 +1,6 @@
 import datetime as dt
 from typing import Callable, Any, TypeVar
-
-
+from functools import wraps
 Func = TypeVar('Func', bound=Callable[..., Any])
 
 
@@ -15,21 +14,24 @@ def cache_results(storage_time: int = 60) -> Callable[[Func], Func]:
     #       'result': <return value of func(args, kwargs)>
     #   }
     # }
-    cache = dict()
 
     def _decorator(func: Func) -> Func:
+        # make cache accessible from the function
+        cache = func.cache = dict()
+        @wraps(func)
         def _wrapper(*args: Any, **kwargs: Any) -> Any:
             cache_key = (args, frozenset(kwargs.items()))
-
             # if function was called with the same arguments
             # less than storage_time seconds ago, return the cached result
             if cache_key in cache:
                 if dt.datetime.now() - cache[cache_key]['timestamp'] <= dt.timedelta(seconds=storage_time):
                     return cache[cache_key]['result']
             # otherwise, run function and store result in cache
+            print(f'new cache at cache_key: {cache_key}')
             cache[cache_key] = dict()
             cache[cache_key]['timestamp'] = dt.datetime.now()
+            result = func(*args, **kwargs)
             cache[cache_key]['result'] = func(*args, **kwargs)
-            return cache[cache_key]['result']
+            return result
         return _wrapper
     return _decorator
